ea9eae01f156657d163c108b16255c080cd2a707
artteeth <0artteeth@gmail.com>
2026-06-17 18:23:04 +0800

    修复 GitHub Pages 路径

--- chat-app/vite.config.js
+++ chat-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Abyss-Front-Controller/',  // 加上这一行，名字和你仓库名一致
+  base: '/Abyss-Front-Controller/', 
 })
